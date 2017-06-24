7807ee2e215b8af89ca5c23e2f29ddebea91c1bd
SenorgCC <Alex.Sinicyn@gmx.de>
2017-06-24 17:37:42 +0200

    anlegen

--- js/dbc/shot.php
+++ js/dbc/shot.php
@@ -1,4 +1,27 @@
 <?php
+$database = 'SHOOTERS';
+$user = 'db2p50';
+$password = 'db2p50';
+$hostname='localhost';
+$port = 60000;
+$conn = db2_connect($database, $user, $password);
+$name = $_POST['name'];
+$zutaten = $_POST['zutaten'];
 
 
+$conn_string = "DRIVER={IBM DB2 ODBC  DRIVER};DATABASE=$database;PORT=$port;HOSTNAME=$hostname;".
+"PROTOCOL=TCPIP;UID=$user;PWD=$password;";
+
+$conn = db2_connect($conn_string, '', '');
+if (!$conn) {
+	echo "connection to $database failed";
+	echo db2_conn_errormsg();
+	ret;
+	}
+
+$insert="INSERT into SHOOTERS.SHOTS(NAME,ZUTATEN) values ($name,$zutaten);"
+$result = db2_exec($conn, $create);
+if ($result) {
+    print "Successfully created the table.\n";
+}
 ?>
--- js/neuershot.js
+++ js/neuershot.js
@@ -1,3 +1,15 @@
 $document.ready(function () {
+    $('#submit_shot').click(function () {
+        var bewertung=$('input[name = "optradio"]:checked').val();
+        var userID=$('#USER_ID').val();
+        var ShotName=$('#shotname').val();
+        var Zutaten=$('#zutaten').val();
+        var sendedata={name:ShotName, zutaten:Zutaten};
+        //ZUERST NEUEN SHOT HINZUFÜGEN!
+        $.post('js/dbc/shot.php', sendedata, function (data) {
+            data = JSON.parse(data);
+        });
 
+        //DANNACH SHOTBEWERTUNG ABGEBEN
+    });
 });

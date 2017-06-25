9cd509c9bca5a36c1f1e588a02b7d773591377ea
SenorgCC <Alex.Sinicyn@gmx.de>
2017-06-25 16:54:03 +0200

    in work : shotbewertung

--- js/neuershot.js
+++ js/neuershot.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-
+    var userBewertung=[];
     $.post('js/dbc/user_auswahl.php', function (data) {
         data = JSON.parse(data);
 
         $.each(data, function (index, data) {
             //if (value != 0) {
            $('#Userliste').append(
-                '<div class="row">'
-                    +'<div class="col-sm-2" style="background-color:yellow;">'
-                        +'<label id="user">'+data[1]+"#"+data[0]+'</label>'
+                '<div class="row" id="row">'
+                    +'<div id="user" class="col-sm-2" style="background-color:yellow;">'
+                        +'<label >'+data[1]+"#"+data[0]+'</label>'
                     +'</div>'
-                    +'<div class="col-sm-10" style="background-color:pink;">'
+                    +'<div id="Rating" class="col-sm-10" style="background-color:pink;">'
                         +'<label class="radio-inline"><input type="radio" name="optradio" value="0">0</label>'
                         +'<label class="radio-inline"><input type="radio" name="optradio" value="1">1</label>'
                         +'<label class="radio-inline"><input type="radio" name="optradio" value="2">2</label>'
@@ -26,22 +26,33 @@ $(document).ready(function () {
                 +'</div>'
 
                 );
-          
         });
+
+       
     });
+
 
     $('#submit_shot').click(function () {
         var bewertung=$('input[name = "optradio"]:checked').val();
         var userID=$('#USER_ID').val();
         var ShotName=$('#shotname').val();
         var Zutaten=$('#zutaten').val();
+
         var sendedata={name:ShotName, zutaten:Zutaten};
-        //ZUERST NEUEN SHOT HINZUFÜGEN!
         $.post('js/dbc/shot.php', sendedata, function (data) {
             data = JSON.parse(data);
             alert(data);
         });
 
         //DANNACH SHOTBEWERTUNG ABGEBEN
+         var i=0;
+        $('#Userliste > div > #user > label').each(function(){
+                var usertag = $(this).text();
+                var userarray=usertag.split('#');
+                //userBewertung.push(userarray);
+                var bewertung=$(this).children('input[name = "optradio"]:checked').val();
+            });
+        alert(userBewertung);
+        
     });
 });

44b9645aefdede5441ae7aa98e2e449fd1c4fa24
SenorgCC <Alex.Sinicyn@gmx.de>
2017-06-25 13:39:41 +0200

    start: neuer_user anlegen

--- js/neueruser.js
+++ js/neueruser.js
@@ -1,13 +1,17 @@
 $document.ready(function () {
-    $('#submit_shot').click(function () {
-        var userID=$('#USER_ID').val();
-        var Vorname=$('#vorname').val();
-        var Nachname=$('#nachname').val();
-        var Geschlecht=$('#input[name = "sexradio"]:checked').val(); //AB HIER WEISS ICH NICHT WEITER
+    $('#submit_tester').click(function () {
+
+        var Vorname = $('#vorname').val();
+        var Nachname = $('#nachname').val();
+        var Kuerzel=$('#kuerzel').val();
+        var Geschlecht=$('input[name = "sexradio"]:checked').val();
+        var Aktiv=$('#usr_aktiv').is(":checked");
+        alert(Vorname, Nachname, Kuerzel, Geschlecht, Aktiv);
         var sendedata={name:ShotName, zutaten:Zutaten};
         //ZUERST NEUEN SHOT HINZUFÜGEN!
         $.post('js/dbc/shot.php', sendedata, function (data) {
             data = JSON.parse(data);
+            alert(data);
         });
 
         //DANNACH SHOTBEWERTUNG ABGEBEN

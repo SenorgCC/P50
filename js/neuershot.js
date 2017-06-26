0563c58d91616e25b369e217f2bd96d18ec6b8a3
SenorgCC <Alex.Sinicyn@gmx.de>
2017-06-26 18:15:42 +0200

    in work : shotbewertung

--- js/neuershot.js
+++ js/neuershot.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     var userBewertung=[];
+    alert("test!");
     $.post('js/dbc/user_auswahl.php', function (data) {
         data = JSON.parse(data);
 
@@ -11,48 +12,46 @@ $(document).ready(function () {
                         +'<label >'+data[1]+"#"+data[0]+'</label>'
                     +'</div>'
                     +'<div id="Rating" class="col-sm-10" style="background-color:pink;">'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="0">0</label>'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="1">1</label>'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="2">2</label>'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="3">3</label>'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="4">4</label>'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="5">5</label>'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="6">6</label>'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="7">7</label>'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="8">8</label>'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="9">9</label>'
-                        +'<label class="radio-inline"><input type="radio" name="optradio" value="10">10</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="0">0</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="1">1</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="2">2</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="3">3</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="4">4</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="5">5</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="6">6</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="7">7</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="8">8</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="9">9</label>'
+                        +'<label class="radio-inline"><input type="radio" name="optradio'+data[0]+'" value="10">10</label>'
                     +'</div>'
                 +'</div>'
-
                 );
         });
-
-       
     });
 
 
     $('#submit_shot').click(function () {
-        var bewertung=$('input[name = "optradio"]:checked').val();
+        //var bewertung=$('input[name = "optradio"]:checked').val();
         var userID=$('#USER_ID').val();
         var ShotName=$('#shotname').val();
         var Zutaten=$('#zutaten').val();
 
         var sendedata={name:ShotName, zutaten:Zutaten};
-        $.post('js/dbc/shot.php', sendedata, function (data) {
-            data = JSON.parse(data);
-            alert(data);
-        });
+        //$.post('js/dbc/shot.php', sendedata, function (data) {
+        //    data = JSON.parse(data);
+        //    alert(data);
+        //});
 
         //DANNACH SHOTBEWERTUNG ABGEBEN
-         var i=0;
+
         $('#Userliste > div > #user > label').each(function(){
                 var usertag = $(this).text();
                 var userarray=usertag.split('#');
                 //userBewertung.push(userarray);
-                var bewertung=$(this).children('input[name = "optradio"]:checked').val();
+                var inputname="optradio"+userarray[1]
+                inputname = JSON.stringify(inputname);
+                var bewertung=$(this).children('input[name ='+inputname+']:checked').val();
+                alert(bewertung);
             });
-        alert(userBewertung);
-        
     });
 });
